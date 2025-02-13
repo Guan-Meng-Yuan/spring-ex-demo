2c5e2a542b87d1478e4f9072534f32da943fc5a2
关梦园 <guan.meng.yuan@icloud.com>
2025-02-13 09:58:49 +0800

    1

--- src/main/java/com/guanmengyuan/spring/ex/demo/controller/UserController.java
+++ src/main/java/com/guanmengyuan/spring/ex/demo/controller/UserController.java
@@ -4,25 +4,18 @@ import com.guanmengyuan.spring.ex.common.model.dto.req.PageReq;
 import com.guanmengyuan.spring.ex.common.model.dto.res.R;
 import com.guanmengyuan.spring.ex.demo.model.domain.User;
 import com.guanmengyuan.spring.ex.demo.service.UserService;
-import com.guanmengyuan.spring.ex.web.controller.BaseController;
 import com.mybatisflex.core.paginate.Page;
 import com.mybatisflex.core.query.QueryWrapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
-public class UserController extends BaseController<User> {
-    public UserController(UserService userService) {
-        super(userService);
-    }
-
-    @GetMapping
-    @Override
-    public R<Page<User>> page(PageReq pageReq, User user) {
-        return page(pageReq, QueryWrapper.create().where(User::getAccount).eq(user.getAccount()));
-    }
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
 
 
 }
